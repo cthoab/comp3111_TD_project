c543c1ccab8ef09a7efaaffc5dc878db787edb2e
jasonlililililili <skliag@connect.ust.hk>
2019-11-05 17:43:02 +0800

    class Grid : add a boolean call hasTower to indicate whether there is a tower inside Tower stuff modified

--- src/main/java/MapElement/Tower/Catapult.java
+++ src/main/java/MapElement/Tower/Catapult.java
@@ -1,6 +1,25 @@
 package MapElement.Tower;
 
+import Arena.Resources;
+import MapElement.Monster.Monster;
+
 public class Catapult extends Tower {
     public static int BuildCost;
+    public static Catapult BuildCatapult(int damage, int range, Resources resources){
+        if (resources.getResources() >= BuildCost)
+            return new Catapult(damage,range,BuildCost);
+        // We use BuildCost as the first UpgradeCost
+        return null;
+    }
 
+
+    private Catapult(int damage, int range, int UpgradeCost) {
+        super(damage, range, UpgradeCost);
+    }
+
+    @Override
+    protected boolean attack(Monster m, Resources resources) {
+        //TODO
+        return false;
+    }
 }
--- src/main/java/MapElement/Tower/IceTower.java
+++ src/main/java/MapElement/Tower/IceTower.java
@@ -1,6 +1,25 @@
 package MapElement.Tower;
 
+import Arena.Resources;
+import MapElement.Monster.Monster;
+
 public class IceTower extends Tower {
     public static int BuildCost;
+    public static IceTower BuildIceTower(int damage,int range, Resources resources){
+        if (resources.getResources() >= BuildCost)
+            return new IceTower(damage,range,BuildCost);
+        // We use BuildCost as the first UpgradeCost
+        return null;
+    }
 
+    //Damage in IceTower means the speed to slow down, it will not reduce HP.
+
+    private IceTower(int damage, int range, int UpgradeCost) {
+        super(damage, range, UpgradeCost);
+    }
+
+    @Override
+    protected boolean attack(Monster m, Resources resources) {
+        return false;
+    }
 }
