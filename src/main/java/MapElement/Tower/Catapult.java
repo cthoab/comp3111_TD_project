ba58d6bb6a049e8ac11f7c4c497bd06549e37c04
skliag@connect.ust.hk <whatthehelp9>
2019-11-07 01:21:33 +0800

    Small Changes in Catapult

--- src/main/java/MapElement/Tower/Catapult.java
+++ src/main/java/MapElement/Tower/Catapult.java
@@ -4,6 +4,7 @@ import Arena.Resources;
 import MapElement.Monster.Monster;
 
 public class Catapult extends Tower {
+    private int reloadTimeLeft;
     public static int BuildCost;
     public static Catapult BuildCatapult(int damage, int range, Resources resources){
         if (resources.getResources() >= BuildCost)
@@ -12,13 +13,22 @@ public class Catapult extends Tower {
         return null;
     }
 
+    public int ReloadTimeLeft() {
+        return reloadTimeLeft;
+    }
+
+    public void coolDown(){
+        if (reloadTimeLeft >0)
+            reloadTimeLeft --;
+    }
 
     private Catapult(int damage, int range, int UpgradeCost) {
-        super(damage, range, UpgradeCost);
+        super(damage, range, UpgradeCost); reloadTimeLeft = 0;
     }
 
     @Override
     protected boolean attack(Monster m, Resources resources) {
+
         //TODO
         return false;
     }
