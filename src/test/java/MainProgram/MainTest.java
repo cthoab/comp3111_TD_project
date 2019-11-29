d355ce19a8ad0092730c8a27c065cb918a6bd697
cthoab <cthoab@connect.ust.hk>
2019-11-29 16:52:35 +0800

    Test Basic Tower Branch 100%

--- src/test/java/MainProgram/MainTest.java
+++ src/test/java/MainProgram/MainTest.java
@@ -1,4 +1,13 @@
-package MainProgram;
-
-public class MainTest {
-}
+//package MainProgram;
+//
+//import javafx.application.Application;
+//import javafx.fxml.FXMLLoader;
+//import javafx.stage.Stage;
+//import org.junit.Assert;
+//import org.junit.Test;
+//public class MainTest {
+//    @Test
+//    public void test(){
+//
+//    }
+//}
--- src/test/java/MainProgram/MyControllerTest.java
+++ src/test/java/MainProgram/MyControllerTest.java
@@ -32,3 +32,66 @@ public class MyControllerTest extends Application{
 
 
 //This testing is not work!! need to be fixed!!
+
+///**
+// *
+// * You might want to uncomment the following code to learn testFX. Sorry, no tutorial session on this.
+// *
+// */
+package MainProgram;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+
+public class MyControllerTest extends ApplicationTest {
+
+	private Scene s;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Tower Defence");
+        s = new Scene(root, 600, 480);
+        primaryStage.setScene(s);
+        primaryStage.show();
+        MyController appController = (MyController)loader.getController();
+        appController.createArena();
+	}
+
+
+	@Test
+	public void testPlayButton() {
+		clickOn("#buttonPlay");
+		AnchorPane b = (AnchorPane)s.lookup("#paneArena");
+		for (javafx.scene.Node i : b.getChildren()) {
+			if (i.getClass().getName().equals("javafx.scene.control.Label")) {
+				Label h = (Label)i;
+				if (h.getLayoutX() == 20 && h.getLayoutY() == 20)
+					Assert.assertEquals(h.getText(), "M");
+			}
+		}
+	}
+	@Test
+	public void testNextFrameButton() {
+		clickOn("#buttonNextFrame");
+		AnchorPane b = (AnchorPane)s.lookup("#paneArena");
+		for (javafx.scene.Node i : b.getChildren()) {
+			if (i.getClass().getName().equals("javafx.scene.control.Label")) {
+				Label h = (Label)i;
+				if (h.getLayoutX() == 20 && h.getLayoutY() == 20)
+					Assert.assertEquals(h.getText(), "M");
+			}
+		}
+	}
+}
